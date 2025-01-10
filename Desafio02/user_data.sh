8d1be491102301222aa4c38669728f5cfb0c5c7e
hpe-palermo <hpalermoemerick@gmail.com>
2025-01-10 15:08:09 -0300

    finalizado atividades de k8s

--- Desafio02/user_data.sh
+++ Desafio02/user_data.sh
@@ -1,36 +1,43 @@
 #!/bin/bash
 
+touch iniciando-instalacao.txt
 # Variáveis de ambiente
-export ENDERECO_EFS="fs-0b3c3a8dfa74ffbe8.efs.us-east-1.amazonaws.com"
+export ENDERECO_EFS="fs-069a7f7a85ca8acec.efs.us-east-1.amazonaws.com"
 export WORDPRESS_DB_HOST="database-1.c1ue0wu6y89k.us-east-1.rds.amazonaws.com"
 export WORDPRESS_DB_USER="admin"
 export WORDPRESS_DB_PASSWORD="nduhr02-02ffk4"
 export WORDPRESS_DB_NAME="db_wp"
+export DIRETORIO_EFS="/mnt/dados"
 
 # Instalar Docker
 sudo yum update -y
 sudo yum install -y docker
 sudo service docker start
+touch docker-instalado.txt
+
+#
+UUID=3a8023d8-2841-47f7-985b-ab4b5825ae27     /           xfs    defaults,noatime  1   1
+UUID=A012-E846        /boot/efi       vfat    defaults,noatime,uid=0,gid=0,umask=0077,shortname=winnt,x-systemd.automount 0 2
+fs-069a7f7a85ca8acec.efs.us-east-1.amazonaws.com:/ /mnt/dados nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport 0 0
 
 # Instalar Docker Compose
-sudo su
 sudo chmod 777 /usr/local/bin/docker-compose
 curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+touch docker-compose-instalado.txt
 
 # Instalar NFS
 sudo yum -y install nfs-utils
-sudo systemctl enable nfs-utils
 sudo systemctl start nfs-utils
+sudo systemctl enable nfs-utils
+touch nfs-instalado.txt
 
 # Montagem do sistema de arquivos
-sudo mkdir -p /mnt/dados
-sudo chmod -R 777 /mnt/dados
-echo "#
-UUID=3a8023d8-2841-47f7-985b-ab4b5825ae27     /           xfs    defaults,noatime  1   1
-UUID=A012-E846        /boot/efi       vfat    defaults,noatime,uid=0,gid=0,umask=0077,shortname=winnt,x-systemd.automount 0 2
-$ENDERECO_EFS:/ /mnt/dados nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport 0 0" | sudo tee  /etc/fstab
+sudo mkdir -p $DIRETORIO_EFS
+sudo chmod -R 777 $DIRETORIO_EFS
+echo "$ENDERECO_EFS:/ $DIRETORIO_EFS nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport 0 0" | sudo tee -a /etc/fstab
 sudo mount -a
+touch efs-montado.txt
 
 # Iniciar o WordPress com Docker Compose
 cat <<EOF > docker-compose.yml
@@ -48,17 +55,12 @@ services:
       WORDPRESS_DB_PASSWORD: $WORDPRESS_DB_PASSWORD
       WORDPRESS_DB_NAME: $WORDPRESS_DB_NAME
     volumes:
-      - /mnt/dados:/var/www/html
+      - $DIRETORIO_EFS:/var/www/html
 EOF
+touch yml-escrito.txt
 
 # Subir aplicação
-docker-compose down
-docker-compose up -d
+sudo docker-compose down
+sudo docker-compose up -d
 
-touch feito.txt
-
-# Instalr MySQL
-# sudo wget https://dev.mysql.com/get/mysql80-community-release-el9-1.noarch.rpm
-# sudo yum install mysql80-community-release-el9-1.noarch.rpm -y 
-# sudo yum install mysql-community-server -y
-# sudo systemctl start mysqld
+touch feito-v11.txt
